ae47af389433f4afefc355fe77bff5454e550ed6
BouncingBallisAFK <vancemahoney1@gmail.com>
2025-06-08 21:09:14 -0400

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -3,5 +3,5 @@ import reflex as rx
 config = rx.Config(
     app_name="nova",
     plugins=[rx.plugins.TailwindV3Plugin()],
-    api_url="http://nova-lemon-omega.vercel.app:8000",
+    REFLEX_api_url="http://nova-lemon-omega.vercel.app:8000",
 )
